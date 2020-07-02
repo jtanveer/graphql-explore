b98186e0a9a9fe7b118cdb6a9b48658f92bd2ab2
Jamael Tanveer <jamael@robi.com.bd>
2020-07-02 12:06:12 +0600

    connect mongodb, sqlite

---
+++ data/resolvers.js
@@ -0,0 +1,31 @@
+import { Friends } from './db-connectors'
+
+export const resolvers = {
+    Query: {
+        getFriend: ({ id }) => {
+            return new Friend(id, friendDatabase[id]);
+        },
+    },
+    Mutation: {
+        createFriend: (root, { input }) => {
+            const newFriend = new Friends({
+                firstName: input.firstName,
+                lastName: input.lastName,
+                gender: input.gender,
+                age: input.age,
+                language: input.language,
+                email: input.email,
+                contacts: input.contacts
+            });
+
+            newFriend.id = newFriend._id;
+
+            return new Promise((resolve, object) => {
+                newFriend.save((err) => {
+                    if (err) reject(err);
+                    else resolve(newFriend);
+                });
+            });
+        },
+    },
+};
